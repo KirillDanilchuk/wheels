6f954a2d9757fdb0258526e7637fd3a3975bb03d
Kirill Danilchuk <kirill.danilchuk01@gmail.com>
2022-05-07 13:16:28 +0300

    Append error test sample

---
+++ samples/failure_test/test.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Kirill Danilchuk <kirill.danilchuk01@gmail.com> on 03/05/2022.
+//
+
+#include "wheels/test/test_framework.hpp"
+
+TEST_SUITE(JustSuite) {
+  TEST(JustTest) {
+    // pass
+  }
+}
+
+TEST_SUITE(Assert) {
+  TEST(True) {
+    ASSERT_TRUE(true);
+  }
+  TEST(False) {
+    ASSERT_FALSE(false);
+  }
+  TEST(Eq) {
+    ASSERT_EQ(1, 1);
+  }
+  TEST(NotEq) {
+    ASSERT_NOT_EQ(1, 1 + 1);
+  }
+  TEST(Less) {
+    ASSERT_LESS(1, 2);
+  }
+  TEST(LessOrEq) {
+    ASSERT_LESS_OR_EQ(1, 2);
+    ASSERT_LESS_OR_EQ(1, 1);
+  }
+  TEST(Great) {
+    ASSERT_GREAT(2, 1);
+  }
+  TEST(GreatOrEq) {
+    ASSERT_GREAT_OR_EQ(2, 1);
+    ASSERT_GREAT_OR_EQ(2, 2);
+  }
+
+  TEST(WithMessage) {
+    ASSERT_EQ(1, 1);
+  }
+
+  TEST(Failure) {
+    ASSERT_TRUE(false);
+  }
+}
+
+RUN_ALL_TESTS();
